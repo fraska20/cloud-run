7b8d4bdd5402c49207cd47281e2a51faa3532747
Fraska <58796786+fraska20@users.noreply.github.com>
2023-06-14 23:32:55 +0700

    update develop

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello</h1><h1>Welcome to Production Page</h1>");
+    res.send("<h1>Hello</h1><h1>Welcome to Developing</h1>");
   });
   
   app.listen(8080, () => {
